7b943334bd9a466cbf305e9d5468af3d5e2c3e2f
Hikita <hsotaro@147ddabc18e3.ant.amazon.com>
2021-01-24 23:35:44 +0900

    完成！

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+// ---------------↓ここから追記↓---------------
+import Amplify, * as AmplifyModules from 'aws-amplify'
+// @ts-ignore
+import { AmplifyPlugin } from 'aws-amplify-vue'
+// @ts-ignore
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
 
-Vue.config.productionTip = false;
+Vue.use(AmplifyPlugin, AmplifyModules)
+// ---------------↑ここまで追記↑---------------
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount('#app');
+  store,
+  render: h => h(App)
+}).$mount('#app')
